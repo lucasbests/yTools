bd47d709fa772f9e5d604b9b77d4f26d42ab408e
fanpifeng <1193504352@qq.com>
2023-05-27 14:08:09 +0800

    project init

---
+++ src/main/java/com/luc/toolscode/ToolsCodeApplication.java
@@ -0,0 +1,16 @@
+package com.luc.toolscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolsCodeApplication.class, args);
+
+
+
+	}
+
+}
---
+++ src/test/java/com/luc/toolscode/ToolsCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luc.toolscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
