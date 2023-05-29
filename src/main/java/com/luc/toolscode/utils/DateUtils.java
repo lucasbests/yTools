6e07123d203b39d24fcd19788bb3e625c3afba19
fanpifeng <1193504352@qq.com>
2023-05-29 09:13:22 +0800

    月份刻度

--- src/main/java/com/luc/toolscode/utils/DateUtils.java
+++ src/main/java/com/luc/toolscode/utils/DateUtils.java
@@ -6,6 +6,7 @@ import cn.hutool.core.date.DateTime;
 import cn.hutool.core.date.DateUtil;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -24,7 +25,28 @@ public class DateUtils extends DateUtil{
         SimpleDateFormat format = new SimpleDateFormat(DatePattern.NORM_DATE_PATTERN);
         List<DateTime> dateTimes = rangeToList(beginTime, endTime, DateField.DAY_OF_MONTH);
         return dateTimes.stream().map(format::format).collect(Collectors.toList());
+    }
 
+
+    /**
+     * 获取时间刻度
+     * @param beginTime
+     * @param endTime
+     * @return
+     */
+    public static List<String> getMonthTimescale(Date beginTime, Date endTime,String pattern){
+        SimpleDateFormat format = new SimpleDateFormat(DatePattern.NORM_DATE_PATTERN);
+        Calendar calendar = Calendar.getInstance();
+
+        List<String> result = new ArrayList<>();
+        while (beginTime.before(endTime)){
+            result.add(format.format(beginTime));
+
+            calendar.setTime(beginTime);
+            calendar.add(Calendar.MONTH,1);
+            beginTime = calendar.getTime();
+        }
+        return result;
     }
 
 
@@ -39,11 +61,6 @@ public class DateUtils extends DateUtil{
         calendar.add(Calendar.DAY_OF_MONTH,p);
         return calendar.getTime();
     }
-
-
-
-
-
 
 
 }
